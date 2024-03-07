05b43478fcf8387bed96588a1ac975971ae21c0b
Mohammad abrar <abrard855@gmail.com>
2024-03-07 14:15:48 +0530

    pointer variable can store the address of a pointer variable

--- first.c
+++ first.c
@@ -1,12 +1,43 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
+  // int a =10;
+  // int *ptr=&a;
+  // printf("the value of a is %d \n",a);
+  // printf("the value of a is %d \n" , *ptr);
+  // printf("the value of a is %d \n" , &a);
+  // printf("the value of a is %d \n" , &*&a);
+
+
+//  printing addresses of a array stored elements
+  // int arr[10];
+  // for(int i=0;i<10;i++){
+  //    printf("the address of arr[%d] =%p \n",i,&arr[i]);
+  // }
+
+
+
   int a =10;
-  int *ptr=&a;
-  printf("the value of a is %d \n",a);
-  printf("the value of a is %d \n" , *ptr);
-  printf("the value of a is %d \n" , &a);
-  printf("the value of a is %d \n" , &*&a);
+  // printf("the vlue of a =%d \n",a);
+  // printf("the address  of a = %p \n",&a);
+  // printf("the value  of a = %d \n",*(&a));
+
+  int *b;
+  b=&a;
+  printf("%p\n",b);
+  printf("%d\n",*b);
+
+  int **c;
+  c=&b;
+  printf("%p\n",c);
+  printf("%d\n",*(*c));
+
+  int ***d;
+  d=&c;
+  printf("%p\n",d);
+  printf("%d\n",*(*(*d)));
+
+
   return 0;
 }
 
