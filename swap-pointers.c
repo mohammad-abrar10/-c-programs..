07442a52dc7d470156b02f0013f810260f72aade
Mohammad abrar <abrard855@gmail.com>
2024-03-09 11:26:14 +0530

    call by referne using pointers

---
+++ swap-pointers.c
@@ -0,0 +1,20 @@
+// call by reference without pointers 
+#include <stdio.h>
+void swap(int *a ,int *b);
+
+int main(int argc, char const *argv[])
+{
+  int a=10;
+  int b=20;
+  printf("the value of a before function =%d and b =%d \n",a,b);
+  swap(&a,&b);
+  printf("the value of a after function =%d and b =%d \n",a,b);
+
+  return 0;
+}
+void swap(int *a ,int *b){
+  int temp =*a;
+  *a=*b;
+  *b=temp;
+  printf("the value of a in fuction is = %d and b =%d \n",*a,*b);
+}
